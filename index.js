771131ad3abb9c76d78f79d5b2d217c2898d2391
Christopher Perez <ChrisAmadeusPerez@gmail.com>
2022-06-09 20:27:08 -0400

    commit tree db3b9b2bb4a4c8af9b5dbbf972539391b8854c8c

--- index.js
+++ index.js
@@ -1,11 +1,41 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(distance){
+
+let result;
+
+if (distance <= 400) {
+  result = 'This one is on me!';
+} else if (distance >= 2000 && distance <= 2499) {
+  result = 'I will gladly take your thirty bucks.';
+} else if (distance >= 2500) {
+  result = 'No can do.';
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+return result;
+
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function ternaryCheckCity(city){
+  
+let response;
+
+city === 'NYC' ? response = 'Ok, sounds good.' : response = 'No go.';
+
+return response;
+
+}
+
+function switchOnCharmFromTip(tip){
+
+let reply;
+
+switch (tip) {
+  case 'generous':
+    return reply = 'Thank you so much.';
+  case 'not as generous':
+    return reply = 'Thank you.';
+  default:
+    return reply = 'Bye.';
+    break;
+}
+
 }
